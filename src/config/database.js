5509adb08614bf5e88ac067f132fd538c14c3789
AbhishekKadam11 <abhishek.kadam007@gmail.com>
2021-03-14 14:11:20 +0530

    Added env file

--- src/config/database.js
+++ src/config/database.js
@@ -2,4 +2,5 @@
 module.exports = {
     'secret': 'AbhishekIsAwesome',
     'database': process.env.MONGODB_URI || 'mongodb://localhost/newTech'
+    // 'database': process.env.MONGODB_URI || 'mongodb+srv://abhishekkadam007:newtech%402@cluster0.fvdcp.mongodb.net/newTech'
   };
