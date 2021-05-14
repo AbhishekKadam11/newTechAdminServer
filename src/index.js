fed7cac5ac302cf835317802925c239a2fc1a3c9
AbhishekKadam11 <abhishek.kadam007@gmail.com>
2021-05-14 18:23:57 +0530

    working on cors issue

--- src/index.js
+++ src/index.js
@@ -10,7 +10,9 @@ const multer = require('multer');
 const Gridfs = require('multer-gridfs-storage');
 const bodyparser = require('body-parser');
 const Grid = require('gridfs-stream');
+const cors = require('cors');
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/.netlify/functions/index', routes);  
 app.use('/', routes);
@@ -22,21 +24,10 @@ var server = app.listen(server_port, function() {
   console.log('Listening on port %d', server_port, 'host at:',host);
 });
 
-//gridfs variable
-// var gfs;
-//----------connect to database------------------- 
-
-// mongoose.connect(config.database, { useNewUrlParser: true, useUnifiedTopology: true});
-// var db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'connection error:'));
-// db.once('open', function callback() {
-//   gfs = Grid(db.db,mongoose.mongo);
-//     console.log("mongoose connected");
-// });
-
 //CORS middleware 
 var allowCrossDomain = function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
+  req.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next(); 
@@ -44,4 +35,3 @@ var allowCrossDomain = function (req, res, next) {
 app.use(allowCrossDomain);
  
 module.exports.handler = serverless(app);
-// module.exports.gfs = gfs;
