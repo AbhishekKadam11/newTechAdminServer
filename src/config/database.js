7933772e3d9ea32fadeb82d8cc5b47b69babb71d
AbhishekKadam11 <abhishek.kadam007@gmail.com>
2021-05-26 20:03:28 +0530

    working on qovery deploy

--- src/config/database.js
+++ src/config/database.js
@@ -3,4 +3,5 @@ module.exports = {
     'secret': 'AbhishekIsAwesome',
     // 'database': process.env.MONGODB_URI || 'mongodb://localhost:27017/newTech'
     'database': process.env.MONGODB_URI || 'mongodb+srv://abhishekkadam007:newtech%402@cluster0.fvdcp.mongodb.net/newTech'
+    // 'database': process.env.MONGODB_URI || 'mongodb://root:naXzhNlNYVSGcOxI@newtech-xmkbalyzx4opt66v-svc.qovery.io:27017/newtech' 
   };
